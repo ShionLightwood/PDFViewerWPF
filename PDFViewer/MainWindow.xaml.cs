13f094b3575be3ec45c3f552c28d778939a35a93
shionlightwood@gmail.com <shionlightwood@gmail.com>
2020-10-02 10:33:50 +0200

    Add project files.

---
+++ PDFViewer/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using MoonPdfLib;
+using MoonPdfLib.MuPdf;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PDFViewer
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Image myImage;
+        MoonPdfPanel pdfPanel = new MoonPdfPanel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            string path = @"C:\Users\Ruhl\Projekte\A17_FlightPlan.pdf";
+            byte[] pdfBytes = File.ReadAllBytes(path);
+            var source = new MemorySource(pdfBytes);
+            moonPdfPanel.Open(source);
+            var pdfsource = moonPdfPanel.CurrentSource;
+            //foreach(var item in )
+
+            //pdfPanel.Open(source);
+        }
+    }
+}
